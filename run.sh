84d2b968b635e73b1d94230be8c9c793c80257e8
Justin Schmidt <jschmidtyyy@hotmail.com>
2017-04-25 13:07:48 -0400

    run.sh updated to mount and umount also switch.py
    upadted run.sh to mount and unomunt sda1 and working pin program hope for no brick this time!

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+sleep 0.1
+mount /dev/sda1 /media/usb0
+sleep 1
 ./test dataoutput.txt settings.txt
 sleep 3
 gnuplot plot
 mv -f index.html /GlucoseAnalyzer/site/
 aws s3 sync /GlucoseAnalyzer/site/ s3://michaelgdonahue.com/
+sleep 0.2
+umount /media/usb0
